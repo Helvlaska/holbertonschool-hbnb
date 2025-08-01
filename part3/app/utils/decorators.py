aed56076831677e4d353cb2a424f3a59302d5fa1
Helvlaska <ccastan@live.fr>
2025-08-01 14:28:01 +0200

    :hammer: (decorators.py): Enhance error handling in handle_errors decorator to return JSON responses for exceptions

--- part3/app/utils/decorators.py
+++ part3/app/utils/decorators.py
@@ -1,16 +1,30 @@
 from functools import wraps
+from flask import jsonify
+from jwt.exceptions import ExpiredSignatureError
+import traceback
 
 def handle_errors(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         try:
             return f(*args, **kwargs)
+        except ExpiredSignatureError:
+            print("⚠️ Token JWT expiré")
+            return jsonify({"message": "Token has expired"}), 401
+
         except ValueError as e:
+            print("❌ ValueError attrapée :", e)
             if 'not found' in str(e).lower():
-                return {'error': str(e)}, 404
-            return {'error': str(e)}, 400
+                return jsonify({'error': str(e)}), 404
+            return jsonify({'error': str(e)}), 400
+
         except TypeError as e:
-            return {'error': str(e)}, 400
+            print("❌ TypeError attrapée :", e)
+            return jsonify({'error': str(e)}), 400
+
         except Exception as e:
-            return {'error': f'Unexpected error: {str(e)}'}, 400
+            print("💥 Erreur inattendue :", e)
+            traceback.print_exc()
+            return jsonify({'error': f'Unexpected error: {str(e)}'}) , 400
+
     return decorated
